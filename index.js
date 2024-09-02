19a8c783d37316aeb7c73fa075d63f32fb8b250f
Tushar <142966652+tusharrajput51220@users.noreply.github.com>
2024-09-02 09:43:51 +0530

    lop

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "https://job-portal-omega-five.vercel.app",
+  origin: ["https://job-portal-five-rho.vercel.app/"],
   // origin: process.env.CLIENT_URL || "http://localhost:3000",
   credentials: true,
 };
