0f1801aef79a7a3b2780b59186705b1e044772d6
Tushar <tusharrajput51220@gmail.com>
2024-10-16 11:11:45 +0530

    new

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -97,19 +97,19 @@ export const login = async (req, res) => {
     });
     const isProduction = req.protocol === "https";
 
-    // const cookieOptions = {
-    //   maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
-    //   httpOnly: true,
-    //   sameSite: "strict",
-    //   secure: isProduction,
-    //   // secure: process.env.NODE_ENV === "production",
-    // };
     const cookieOptions = {
       maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
-      httpOnly: true, // This ensures the cookie is not accessible via JavaScript
-      sameSite: "lax", // "lax" is good for local development; "strict" can cause issues with certain redirects
-      secure: isProduction, // Disable secure flag for local testing, since it's not using HTTPS
+      httpOnly: true,
+      sameSite: "strict",
+      // secure: isProduction,
+      secure: process.env.NODE_ENV === "production",
     };
+    // const cookieOptions = {
+    //   maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+    //   httpOnly: true, // This ensures the cookie is not accessible via JavaScript
+    //   sameSite: "lax", // "lax" is good for local development; "strict" can cause issues with certain redirects
+    //   secure: isProduction, // Disable secure flag for local testing, since it's not using HTTPS
+    // };
 
     res
       .status(200)
